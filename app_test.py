b5f3ad5460bdf7e9b16de625ffdc953398eb11b1
ibugueye <34722918+ibugueye@users.noreply.github.com>
2024-04-05 01:30:09 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -86,7 +86,7 @@ pages = ["Contexte du projet", "Exploration des données", "Analyse de données"
 page = st.sidebar.radio("Aller vers la page :", pages)
 #df_train = pd.read_csv("data/application_train.csv")
 #df_test = pd.read_csv("data/application_test.csv")
-df = pd.read_csv("data/df_final.csv")
+df = pd.read_csv("df_final.csv")
 df["CODE_GENDER"].fillna(0, inplace=True)
 if page == pages[0]:
     st.write('### Contexte du projet')
